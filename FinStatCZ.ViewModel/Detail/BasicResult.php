df5f5ab04abdf0d751d44fbb8723a8fe79268090
Marek Stipek <stipek@shoptet.cz>
2022-12-07 22:16:42 +0100

    - Introduced composer package - Moved CZ API into a separate namespace to prevent class collisions - Fixed minor typo CzNaceDicision -> CzNaceDivision

--- FinStatCZ.ViewModel/Detail/BasicResult.php
+++ FinStatCZ.ViewModel/Detail/BasicResult.php
@@ -1,8 +1,10 @@
 <?php
-require_once('../FinStat.Client/ViewModel/AddressResult.php');
-require_once('../FinStat.Client/ViewModel/Detail/AbstractResult.php');
+namespace FinstatApiCz;
 
-class BasicResult extends AbstractResult
+require_once(__DIR__ . '/../../FinStat.Client/ViewModel/AddressResult.php');
+require_once(__DIR__ . '/../../FinStat.Client/ViewModel/Detail/AbstractResult.php');
+
+class BasicResult extends \AbstractResult
 {
 }
 ?>
